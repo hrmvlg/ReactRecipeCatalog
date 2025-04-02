96712e3dea04d4a5fcc5adfacc3e9d78ecfbd7b3
Olya Khromova <olya.hrmvlg@gmail.com>
2025-04-02 17:21:43 +0300

    Revert "update footer and header links to point to the GitHub repository"
    This reverts commit c3b0283059007285e6b67a0249902634361e2eac.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,12 +3,7 @@ export default function Header() {
     return (
         <nav className=" yellow darken-2">
             <div className="nav-wrapper">
-                <a
-                    className="brand-logo left"
-                    href="https://github.com/hrmvlg/ReactRecipeCatalog"
-                >
-                    React Recipe Catalog
-                </a>
+                <a href="/" className="brand-logo left">React Recipe Catalog</a>
                 <ul id="nav-mobile" className="right">
                     <li>
                         <a href="">GitHub Repository</a>
